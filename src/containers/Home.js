7ad2a40cc3f18b80cf2258f4690bc0c4539ad9f1
Alejandro Villarreal <avillarreal01@gmail.com>
2018-02-20 18:54:07 -0500

    add styled landing page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react'
 
+import './Home.css'
+
 class Home extends Component {
+  handleChange(evt) {
+    evt.preventDefault()
+    alert('ayo')
+  }
+
   render() {
     return (
-      <section className="hero is-dark is-large">
-        <div className="hero-body">
-          <div className="container">
-            <h1 className="title">Welcome to GitViewer</h1>
-            <h2 className="subtitle">
-              Where all your hopes and dreams come true.
-            </h2>
-          </div>
+      <div className="Home">
+        <div className="container">
+          <h1 className="title">Welcome to GitViewer</h1>
+          <h2 className="subtitle">
+            Look through Github repos with file trees and full search.
+          </h2>
+          <form onSubmit={this.handleChange}>
+            <div className="field">
+              <div className="control">
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="Go on, check it out!"
+                />
+              </div>
+            </div>
+            <div className="field">
+              <div className="control" style={{textAlign: 'center'}}>
+                <button type="submit" className="button is-light is-outlined">
+                  Search
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
-      </section>
+      </div>
     )
   }
 }
