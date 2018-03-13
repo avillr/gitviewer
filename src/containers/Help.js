f32bf1dbf456565591842e168da40c5a1fb6ec4d
Alex Villarreal <avillarreal01@gmail.com>
2018-03-13 18:42:20 -0400

    add help page

--- src/containers/Help.js
+++ src/containers/Help.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
 const Help = props => {
+  const issueTitle = 'Check out this error!'
   return (
-    <div className="container">
-      <div className="content">
-        <h1>Help</h1>
+    <div className='Help'>
+      <div className='container'>
+        <h1 className='subtitle is-1'>Help</h1>
+        <p>
+          Sorry something broke, if you make an issue <a href={`https://github.com/alexv/gitviewer/issues/new?title=${issueTitle}`} >here</a> I'll fix it asap!
+        </p>
       </div>
     </div>
   )
 }
 
 export default Help
-
